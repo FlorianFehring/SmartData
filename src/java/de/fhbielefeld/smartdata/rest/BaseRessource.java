25a7e47a9d8e233b647b18f12376bdca5a3d996a
Florian Fehring <Florian Fehring@DESKTOP-SKAFEOK.dhcp.fh-bielefeld.de>
2020-09-07 10:19:11 +0200

    Added: REST interface for createing and deleteing schemata

--- src/java/de/fhbielefeld/smartdata/rest/BaseRessource.java
+++ src/java/de/fhbielefeld/smartdata/rest/BaseRessource.java
@@ -12,7 +12,9 @@ import javax.annotation.Resource;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -47,6 +49,59 @@ public class BaseRessource {
         } catch (LoggerException ex) {
             System.err.println("Error init logger: " + ex.getLocalizedMessage());
         }
+    }
+
+    @POST
+    @Path("createSchema")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Creates a new schema",
+            description = "Creates a new schema in database")
+    @APIResponse(
+            responseCode = "201",
+            description = "Schema was created")
+    @APIResponse(
+            responseCode = "304",
+            description = "Schema already exists")
+    @APIResponse(
+            responseCode = "400",
+            description = "Error mesage",
+            content = @Content(mediaType = "application/json",
+                    example = "{\"errors\" : [ \"No schema name given.\"]}"))
+    @APIResponse(
+            responseCode = "500",
+            description = "Error mesage",
+            content = @Content(mediaType = "application/json",
+                    example = "{\"errors\" : [ \" Could not create schema: Because of ... \"]}"))
+    public Response createSchema(@Parameter(description = "Schema name", required = true,
+            schema = @Schema(type = STRING, defaultValue = "public")
+    ) @QueryParam("schema") String schema) {
+        ResponseObjectBuilder rob = new ResponseObjectBuilder();
+
+        if (schema == null) {
+            rob.setStatus(Response.Status.BAD_REQUEST);
+            rob.addErrorMessage("No schema name given.");
+            return rob.toResponse();
+        }
+
+        Connection con = this.getConnection(rob);
+        if (con == null) {
+            return rob.toResponse();
+        }
+
+        DynBase db = new DynBasePostgres(con);
+        try {
+            if (db.createSchemaIfNotExists(schema)) {
+                rob.setStatus(Response.Status.CREATED);
+            } else {
+                rob.setStatus(Response.Status.NOT_MODIFIED);
+            }
+        } catch (DynException ex) {
+            rob.setStatus(Response.Status.INTERNAL_SERVER_ERROR);
+            rob.addErrorMessage("Error retriving table names: " + ex.getLocalizedMessage());
+            rob.addException(ex);
+        }
+
+        return rob.toResponse();
     }
 
     @GET
@@ -91,6 +146,59 @@ public class BaseRessource {
         }
 
         rob.setStatus(Response.Status.OK);
+        return rob.toResponse();
+    }
+
+    @DELETE
+    @Path("deleteSchema")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Deletes a schema",
+            description = "Deletes a schema and all its contents")
+    @APIResponse(
+            responseCode = "200",
+            description = "Schema was deleted")
+    @APIResponse(
+            responseCode = "304",
+            description = "Schema was not existend")
+    @APIResponse(
+            responseCode = "400",
+            description = "Error mesage",
+            content = @Content(mediaType = "application/json",
+                    example = "{\"errors\" : [ \"No schema name given.\"]}"))
+    @APIResponse(
+            responseCode = "500",
+            description = "Error mesage",
+            content = @Content(mediaType = "application/json",
+                    example = "{\"errors\" : [ \" Could not delete schema: Because of ... \"]}"))
+    public Response deleteSchema(@Parameter(description = "Schema name", required = true,
+            schema = @Schema(type = STRING, defaultValue = "public")
+    ) @QueryParam("schema") String schema) {
+        ResponseObjectBuilder rob = new ResponseObjectBuilder();
+
+        if (schema == null) {
+            rob.setStatus(Response.Status.BAD_REQUEST);
+            rob.addErrorMessage("No schema name given.");
+            return rob.toResponse();
+        }
+
+        Connection con = this.getConnection(rob);
+        if (con == null) {
+            return rob.toResponse();
+        }
+
+        DynBase db = new DynBasePostgres(con);
+        try {
+            if (db.deleteSchema(schema)) {
+                rob.setStatus(Response.Status.OK);
+            } else {
+                rob.setStatus(Response.Status.NOT_MODIFIED);
+            }
+        } catch (DynException ex) {
+            rob.setStatus(Response.Status.INTERNAL_SERVER_ERROR);
+            rob.addErrorMessage("Error retriving table names: " + ex.getLocalizedMessage());
+            rob.addException(ex);
+        }
+
         return rob.toResponse();
     }
 
