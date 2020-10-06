a86a40ac71dd52e2bc164ad089b55eeebccdf41a
Florian Fehring <Florian Fehring@DESKTOP-SKAFEOK.dhcp.fh-bielefeld.de>
2020-10-06 10:48:40 +0200

    Changed: Postgres connection now uses configuration Fixed: Postgres connection do not report errors

--- src/java/de/fhbielefeld/smartdata/dyn/DynPostgres.java
+++ src/java/de/fhbielefeld/smartdata/dyn/DynPostgres.java
@@ -3,6 +3,7 @@ package de.fhbielefeld.smartdata.dyn;
 import de.fhbielefeld.scl.logger.Logger;
 import de.fhbielefeld.scl.logger.message.Message;
 import de.fhbielefeld.scl.logger.message.MessageLevel;
+import de.fhbielefeld.smartdata.config.Configuration;
 import de.fhbielefeld.smartdata.exceptions.DynException;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -24,20 +25,27 @@ public class DynPostgres implements Dyn {
     
     @Override
     public void connect() throws DynException {
+        Configuration conf = new Configuration();
+        String jndi = conf.getProperty("postgres.jndi");
+        if(jndi == null)
+            jndi = "jdbc/smartdata";
+        
         try {
             InitialContext ctx = new InitialContext();
-            DataSource ds = (DataSource) ctx.lookup("jdbc/smartdata");
+            DataSource ds = (DataSource) ctx.lookup(jndi);
             this.con = ds.getConnection();
         } catch (NamingException ex) {
             Message msg = new Message("", MessageLevel.ERROR, "Could not access connection pool: " + ex.getLocalizedMessage());
             Logger.addMessage(msg);
             DynException dex = new DynException("Could not access connection pool: " + ex.getLocalizedMessage());
             dex.addSuppressed(ex);
+            throw dex;
         } catch (SQLException ex) {
             Message msg = new Message("", MessageLevel.ERROR, "Could not conntect to database: " + ex.getLocalizedMessage());
             Logger.addMessage(msg);
             DynException dex = new DynException("Could not conntect to database: " + ex.getLocalizedMessage());
             dex.addSuppressed(ex);
+            throw dex;
         }
     }
 
