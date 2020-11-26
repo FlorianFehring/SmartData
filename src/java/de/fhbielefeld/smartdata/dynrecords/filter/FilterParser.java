83b34efc8e37dc4e86fab414d4048b05a59d540c
Alexej Rogalski <alexej.rogalski@fh-bielefeld.de>
2020-11-26 14:31:51 +0100

    Changed: Filter parser

--- src/java/de/fhbielefeld/smartdata/dynrecords/filter/FilterParser.java
+++ src/java/de/fhbielefeld/smartdata/dynrecords/filter/FilterParser.java
@@ -13,49 +13,41 @@ public class FilterParser {
 
     public static Filter parse(String filter, DynCollection table) throws FilterException {
         Filter f = null;
-        if (filter.contains(",cs,")) {
-            Message msg = new Message("SmartData", MessageLevel.ERROR, "Filter for >" + filter + "< is not implemented yet.");
-            Logger.addMessage(msg);
-        } else if (filter.contains(",sw,")) {
-            Message msg = new Message("SmartData", MessageLevel.ERROR, "Filter for >" + filter + "< is not implemented yet.");
-            Logger.addMessage(msg);
-        } else if (filter.contains(",ew,")) {
-            Message msg = new Message("SmartData", MessageLevel.ERROR, "Filter for >" + filter + "< is not implemented yet.");
-            Logger.addMessage(msg);
-        } else if (filter.contains(",eq,")) {
-            f = new EqualsFilter(table);
-            f.parse(filter);
-        } else if (filter.contains(",lt,")) {
-            Message msg = new Message("SmartData", MessageLevel.ERROR, "Filter for >" + filter + "< is not implemented yet.");
-            Logger.addMessage(msg);
-        } else if (filter.contains(",le,")) {
-            Message msg = new Message("SmartData", MessageLevel.ERROR, "Filter for >" + filter + "< is not implemented yet.");
-            Logger.addMessage(msg);
-        } else if (filter.contains(",ge,")) {
-            Message msg = new Message("SmartData", MessageLevel.ERROR, "Filter for >" + filter + "< is not implemented yet.");
-            Logger.addMessage(msg);
-        } else if (filter.contains(",gt,")) {
-            Message msg = new Message("SmartData", MessageLevel.ERROR, "Filter for >" + filter + "< is not implemented yet.");
-            Logger.addMessage(msg);
-        } else if (filter.contains(",bt,")) {
-            Message msg = new Message("SmartData", MessageLevel.ERROR, "Filter for >" + filter + "< is not implemented yet.");
-            Logger.addMessage(msg);
-        } else if (filter.contains(",in,")) {
-            Message msg = new Message("SmartData", MessageLevel.ERROR, "Filter for >" + filter + "< is not implemented yet.");
-            Logger.addMessage(msg);
-        } else if (filter.contains(",is,")) {
-            Message msg = new Message("SmartData", MessageLevel.ERROR, "Filter for >" + filter + "< is not implemented yet.");
-            Logger.addMessage(msg);
-        } else if (filter.contains(",sir,")) {
-            f = new RadiusFilter(table);
-            f.parse(filter);
-        } else if (filter.contains(",sib,")) {
-            f = new BoundingBoxFilter(table);
-            f.parse(filter); 
-        } else {
-            Message msg = new Message("SmartData", MessageLevel.ERROR, "Filter for >" + filter + "< is unkown.");
-            Logger.addMessage(msg);
-        }
+        String[] parts = filter.split(",");
+        String filtername = parts[1];
+        switch (filtername) {
+            case "cs":
+            case "ncs":
+                f = new ContainFilter(table);
+		f.parse(filter);
+                break;
+            case "sw":
+            case "nsw":
+                f = new StartsWithFilter(table);
+		f.parse(filter);
+                break;
+            case "ew":
+            case "new":
+                f = new EndsWithFilter(table);
+		f.parse(filter);
+                break;
+            case "eq":
+            case "neq":
+                f = new EqualsFilter(table);
+		f.parse(filter);
+                break;
+            case "sir":
+                f = new RadiusFilter(table);
+		f.parse(filter);
+                break;
+            case "sib":
+                f = new BoundingBoxFilter(table);
+		f.parse(filter);
+                break;
+            default:
+                Message msg = new Message("SmartData", MessageLevel.ERROR, "Filter for >" + filter + "< is unkown.");
+		Logger.addMessage(msg);
+            }
         return f;
     }
 }
