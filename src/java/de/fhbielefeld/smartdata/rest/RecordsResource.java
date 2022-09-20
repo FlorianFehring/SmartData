58f86b1c2bb314c35c792ba8b97b64ae9e0799f1
Florian Fehring <Florian Fehring@DESKTOP-SKAFEOK.dhcp.fh-bielefeld.de>
2022-09-20 08:37:47 +0200

    Changed: Removed unused import

--- src/java/de/fhbielefeld/smartdata/rest/RecordsResource.java
+++ src/java/de/fhbielefeld/smartdata/rest/RecordsResource.java
@@ -24,7 +24,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.StringJoiner;
 import javax.naming.NamingException;
 import javax.ws.rs.Produces;
